3f9877d3592f7cee01bf8436ed2a5af03f9d0b2e
ssorrrell <ssorrrell@hotmail.com>
2020-07-25 13:23:35 -0500

    Add Client UWP and Android Shell Apps

---
+++ WAppClient/WAppClient/WAppClient/WAppClient/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using WAppClient.Models;
+
+namespace WAppClient.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
